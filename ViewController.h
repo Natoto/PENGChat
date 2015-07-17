a52745e2b124caf343e4b9f752b0412a210cb5c2
Natoto <boob@BooBdeMac-mini.local>
2015-07-17 23:26:19 +0800

    pengchat
    first peng

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PENGChat
+//
+//  Created by BooB on 15/7/17.
+//  Copyright (c) 2015年 BooB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PENGChat
+//
+//  Created by BooB on 15/7/17.
+//  Copyright (c) 2015年 BooB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
